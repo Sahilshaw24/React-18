8b09cc56bea8057fb71dd0c5785de578893f274a
Sahil Shaw <shaws2857@gmail.com>
2026-03-06 11:07:34 +0530

    first

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,25 @@
 import { useState, useEffect } from "react";
 
-const [resturantList, setResturantList] = useState("");
-
-useEffect(() => {
-  fetchdata();
-}, []);
-
-const fetchdata = async () => {
-  const data = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.858427401320995&lng=88.3914329856634&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-  );
-
-  const json = await data.json();
-  console.log(json);
-};
-
 const Body = () => {
+  const [resturantList, setResturantList] = useState([]);
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.858427401320995&lng=88.3914329856634&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setResturantList(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+  };
+
   return (
     <div className="body">
       <div className="search">
@@ -26,6 +30,13 @@ const Body = () => {
       </div>
       <div className="restaurant-list">
         <h2>Restaurant List</h2>
+        <ul>
+          {resturantList.map((restaurant) => (
+            <li key={restaurant.info.id}>
+              {restaurant.info.name} - {restaurant.info.avgRating}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
