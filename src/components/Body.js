0b1a37b4b6a58e65bb547e50163aed7908fb51e7
Sahil Shaw <shaws2857@gmail.com>
2026-03-10 18:28:31 +0530

    first

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,6 @@ const Body = () => {
     );
 
     const json = await data.json();
-    console.log(json);
     setResturantList(
       json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants,
